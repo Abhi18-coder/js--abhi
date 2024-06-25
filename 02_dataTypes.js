6f9870f916847d67b56dd309717eba8ef532521c
Abhinav Tiwari <abhit8568@gmail.com>
2024-06-25 13:25:02 +0000

    dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+// code readiability should be high //
+
+console.log("Abhinav")
+
+let name = "abhinav"
+let age = 18
+let isLoggedin = false
+
+
+// number => 2 to the powere of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined dataType
+console.log(typeof null); // object dataType
